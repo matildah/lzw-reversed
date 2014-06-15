d5f3e4e327df86b8e2ae81a96d9043d1f5ec70b5
Kia <2592730+superbaud@users.noreply.github.com>
2014-06-14 18:57:13 -0700

    lzwctx

--- getclzw.c
+++ getclzw.c
@@ -1,4 +1,63 @@
+/* decompresses the LZW compression used by this awful software.
+
+   copyright (c) 2014 Matilda Helou <heinousbutch@gmail.com>. 
+   licensed under the terms of the ISC license 
+   */
+
 #include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct lzwctx {
+    /* buffers */
+    uint8_t *dict;
+    /* dict is arranged as such:
+       [uint32_t byte] [uint32_t pointer] */
+
+    uint8_t *obuf;
+    size_t obuf_len;
+    size_t obuf_idx;
+
+    /* decoder state */
+    int numsymbols;
+    int symbolwidth; /* no, you can't derive this from numsymbols */
+    int lastsymbol;
+
+    /* status flags */
+    int overfill;
+    int firstrun;
+
+    /* accumulator state */
+    uint32_t accumulator;
+    int bits_in_accumulator;
+};
+
+struct lzwctx *
+initLZW()
+{
+    struct lzwctx *lzw;
+
+    lzw = malloc(sizeof(struct lzwctx));
+    assert(NULL != lzw);
+
+    lzw->dict = malloc(32768); /* yes, i am writing this on a 64 bit machine. #yolo */
+    assert(NULL != lzw->dict);
+
+    lzw->obuf = malloc(8192);
+    assert(NULL != lzw->obuf);
+
+    lzw->numsymbols = 256;
+    lzw->symbolwidth = 9;
+
+    lzw->overfill = 0;
+    lzw->firstrun = 1;
+
+    lzw->accumulator = 0;
+    lzw->bits_in_accumulator = 0;
+    
+    return lzw;
+}
+
 int main()
 {
     uint8_t *buf_32, *buf_16;
